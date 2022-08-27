ffc27d1b2e61cf173818988bf14d0382e9556352
DarioMarret <javier_dario_marret@hotmail.com>
2022-08-27 01:41:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -28,9 +28,7 @@ function App() {
 
   useEffect(() => {
     console.log(window.location);
-    console.log(window.location.search);
-    console.log(decode(window.location.search));
-    // setData(JSON.parse(decode(window.location.search)))
+    setData(JSON.parse(decode(window.location.pathname.split('/pdf/')[1])))
   }, []);
   return (
     <div className="App">
